08c7e16e5ef825cb0c346f4c8b1f3debaa4d2437
Michael Bianco <iloveitaly@gmail.com>
2019-04-14 11:58:43 -0600

    File read working

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -5,75 +5,106 @@ class MintContext {
     constructor() {
         // this.token = $("#javascript-token").value
     }
+
+    token() {
+        // you can't access the page's JS objects, so Mint.getToken() is not available
+        // additionally, javascript-token is not filled in till a while later
+        // best option available to us is pulling the CSRF from the session, which is
+        // accessible across browser windows
+
+        return window.sessionStorage.getItem("CSRF");
+    }
+
+    accountId() {
+        debugger
+    }
 }
 
 class MintIntegrator {
     constructor() {
         this.context = new MintContext();
+        this.addTransactionEndpoint = "https://mint.intuit.com/updateTransaction.xevent";
     }
 
-    addTransaction() {
+    addTransaction(date, merchant, amount) {
+        let transactionData = {
+            "cashTxnType": "on",
+            "mtCashSplit": "on",
+            // mtCheckNo=&
+            "task":"txnadd",
+            "txnId":":0",
+            "mtType":"cash",
+            "mtAccount":"3444067",
+            // symbol=&
+            // note=&
+            "isInvestment":"false",
+            "catId":"7",
+            // category=Food%20%26%20Dining&
 
+            "merchant": merchant,
+            "date": date,
+            "amount": amount,
+
+            "mtIsExpense":"true",
+            "mtCashSplitPref":"1",
+            "token": this.context.token()
+        };
+
+        $.ajax({
+            method: "POST",
+            url: this.addTransactionEndpoint,
+            data: transactionData
+        }).then((data, status) => console.log(data))
     }
 }
 
 $(document).ready(() => {
-    // var integrator = new MintIntegrator();
-    // integrator.addTransaction()
+    // removes the annoying mint ads
+    $('head').append('<style type="text/css">.offerSection { display: none !important; }</style>');
 
-    // $("#controls-top").append('<a class="btn btn-hollow btn-sm" href="javascript://" id="controls-import" title="Import Transactions">Import Transactions</a>')
-    // debugger
-    // let target = $("#body-mint").get();
-    
-    // let observer = new MutationObserver((mutationsList, observer) => console.log(mutationsList));
-    // let config = { attributes: false, childList: false, subtree: true };
-
-    // observer.observe(target, config);
-
-    // Later, you can stop observing
-    // observer.disconnect();
-
-    // function boom() {
-
-    //     if ($("#product-view-root").size() > 0) {
-    //         console.log("retry!")
-    //       window.requestAnimationFrame(boom);
-    //     } else {
-    //         console.log("got it!")
-    //     //    $("#product-view-root").do_some_stuff();
-    //      }
-    //   }
-
-    //   boom()
     // https://swizec.com/blog/how-to-properly-wait-for-dom-elements-to-show-up-in-modern-browsers/swizec/6663
     // https://gist.github.com/chrisjhoughton/7890303
-    /**
- * Wait for the specified element to appear in the DOM. When the element appears,
- * provide it to the callback.
- *
- * @param selector a jQuery selector (eg, 'div.container img')
- * @param callback function that takes selected element (null if timeout)
- * @param maxtries number of times to try (return null after maxtries, false to disable, if 0 will still try once)
- * @param interval ms wait between each try
- */
-function waitForEl(selector, callback, maxtries = false, interval = 100) {
-    const poller = setInterval(() => {
-      const el = $(selector)
-      const retry = maxtries === false || maxtries-- > 0
-      if (retry && el.length < 1) return // will try again
-      clearInterval(poller)
-      callback(el || null)
-    }, interval)
-  }
-
-    // $.when($('#controls-top')).then((self) => {
-    //     debugger
-        // $("#controls-top").append('<a class="btn btn-hollow btn-sm" href="javascript://" id="controls-import" title="Import Transactions">Import Transactions</a>')
-    // });
+    function waitForEl(selector, callback, maxtries = false, interval = 100) {
+        const poller = setInterval(() => {
+            const el = $(selector)
+            const retry = maxtries === false || maxtries-- > 0
+            if (retry && el.length < 1) return // will try again
+            clearInterval(poller)
+            callback(el || null)
+        }, interval)
+    }
 
     waitForEl('#controls-top', (el) => {
-        let importButton = $.parseHTML('<a class="btn btn-hollow btn-sm" href="javascript://" id="controls-import" title="Import Transactions">Import Transactions</a>')
-        $("#controls-top").append(importButton)
+        let fileInput = $.parseHTML('<input type="file" style="display: none" />');
+        let importButton = $.parseHTML('<a class="btn btn-hollow btn-sm" title="Import Transactions">Import Transactions</a>')
+
+        $("#controls-top").append(importButton).append(fileInput)
+
+        // you can only trigger a file load during a user-initiated click event
+        // https://stackoverflow.com/questions/5872815/reading-local-files-with-input-type-file
+        // https://mariusschulz.com/blog/programmatically-opening-a-file-dialog-with-javascript
+
+        $(fileInput).on('change', (evt) => {
+            var f = evt.target.files[0];
+
+            if (f) {
+                var r = new FileReader();
+                r.onload = function(e){          
+                    console.log(e.target.result);
+                };
+
+                r.readAsText(f);
+            } else 
+            {
+                console.log("failed");
+            }
+        })
+
+        $(importButton).on('click', () => {
+            $(fileInput).trigger('click')
+            // var integrator = new MintIntegrator();
+            // integrator.addTransaction("04/12/2019", "ROBOT", "1")      
+        })
     })
 })
 
